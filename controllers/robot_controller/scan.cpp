28144566901effd43353f73566e29d9747848834
ep585 <edprager0@gmail.com>
2021-02-03 16:27:44 +0000

    scan

--- controllers/robot_controller/scan.cpp
+++ controllers/robot_controller/scan.cpp
@@ -1,3 +1,37 @@
 #include "scan.h"
 
-//
+Scan::Scan(Robot *_robot, int _timeStep){
+	robot = _robot;
+	timeStep = _timeStep;
+	
+	dsFront = new SensorDistance(robot, "ds_front", timeStep, 0.1, "ds_front");
+	dsRear = new SensorDistance(robot, "ds_rear", timeStep, 0.1, "ds_rear");
+	camera = new SensorColour(robot, "cs_colour", timeStep, "cs_colour");
+	gps = new SensorGPS(robot, "gps", timeStep, "gps");
+	compass = new SensorCompass(robot, "compass", timeStep, "compass");
+}
+
+vec Scan::ReadPosition(){
+	return gps->getCoordinates();
+}
+
+double Scan::ReadBearing(){
+	return compass->getOrientation().Bearing();
+}
+
+float Scan::ReadFrontDistance(){
+	return dsFront->getDistance();
+}
+
+float Scan::ReadRearDistance(){
+	return dsRear->getDistance();
+}
+
+Colour Scan::ReadColour(){
+	int *rgb = camera->getColour();
+	int blueness = rgb[2] - rgb[0];
+	int deadZone = 20;
+	blueness = (blueness > deadZone) - (blueness < -deadZone);
+	if(blueness) return (blueness > 0 ? blue : red);
+	return dunno;
+}
--- controllers/robot_controller/scan.h
+++ controllers/robot_controller/scan.h
@@ -2,7 +2,27 @@
 #define SCAN
 
 #include "header.h"
+#include "sensor.h"
 
-//
+class Scan {
+public:
+	Scan(Robot *_robot, int _timeStep);
+	
+	vec ReadPosition();
+	double ReadBearing();
+	float ReadFrontDistance();
+	float ReadRearDistance();
+	Colour ReadColour();
+	
+private:
+	Robot *robot;
+	int timeStep;
+
+	SensorCompass *compass;
+	SensorDistance *dsFront;
+	SensorDistance *dsRear;
+	SensorColour *camera;
+	SensorGPS *gps;
+};
 
 #endif
